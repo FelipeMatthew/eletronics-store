7aac2731d4e9fcde3c1ceb2cc39deecb82529cf9
felipematthew <felipematthewnascimento@gmail.com>
2024-01-25 12:37:41 -0300

    ✨ feat: User validation using JWT

--- API/src/routes/user.js
+++ API/src/routes/user.js
@@ -2,9 +2,11 @@ import { Router } from 'express';
 
 import userController from '../controllers/User';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
-router.get('/', userController.listAll);
+router.get('/',  loginRequired, userController.listAll);
 router.get('/:id/', userController.listById);
 router.post('/', userController.create);
 router.put('/:id/', userController.update);
