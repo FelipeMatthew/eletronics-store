9a7c9e65bc09f4db6ce1525982dabcb66820cfb1
felipematthew <felipematthewnascimento@gmail.com>
2024-01-26 13:26:54 -0300

    :sparkles: feat: Multer controller data settings

--- API/src/config/multer.js
+++ API/src/config/multer.js
@@ -6,6 +6,13 @@ const randomNum = () => {
 }
 
 export default {
+  // Aceitar apenas imagens
+  fileFilter: (req, file, cb) => {
+    if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg'){
+      return cb(new multer.MulterError('File extension must be PNG or JPG.'))
+    }
+    cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
--- API/src/controllers/Picture.js
+++ API/src/controllers/Picture.js
@@ -1,12 +1,19 @@
-import { SUCCESS } from '../config/status';
+import { BAD_REQUEST, SUCCESS } from '../config/status';
 
-import Product from '../models/Product';
+import multer from 'multer';
 
+import multerConfig from '../config/multer'
+
+const upload = multer(multerConfig).single('file')
 class PictureController {
   async create(req, res) {
+    return upload(req, res, (err) => {
+      if(err) {
+        return res.status(BAD_REQUEST).json({ errors: err.code })
+      }
+      return res.status(SUCCESS).json(req.file);
+    })
 
-
-    res.status(SUCCESS).json(req.file);
   }
 }
 
